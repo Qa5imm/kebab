af63ad2edbbc9ebfa16599c05f1ff7250d7d3570
Qa5im <24100256@lums.edu.pk>
2023-09-29 03:22:25 +0500

    topQuestions  made responsive

--- components/TopQuestions/TopQuestion.vue
+++ components/TopQuestions/TopQuestion.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="dropdown" class="flex flex-col items-center w-3/5">
+  <div id="dropdown" :class="`flex flex-col items-center w-3/5 ${dropdownMd}`">
     <label
       class="py-3 px-3 w-full bg-bgray-200 flex justify-between items-center cursor-pointer rounded-t-lg mt-4"
       :for="inputId"
@@ -12,20 +12,18 @@
     <input type="checkbox" :id="inputId" class="hidden" />
 
     <div id="menu" :class="` w-full bg-bgray-200 rounded-b-lg`">
-      <p class="w-10/12 py-3 px-3 text-start">{{ answer }}</p>
+      <p class="py-3 px-3 text-start">{{ answer }}</p>
     </div>
   </div>
 </template>
 <script setup>
-
 const { query, answer } = defineProps({
   query: String,
   answer: String,
 });
 
 const inputId = `touch-${Math.random().toString(36).substring(7)}`;
-
-
+const dropdownMd = `max-md:w-full`;
 </script>
 <style scoped>
 #menu {
@@ -36,20 +34,6 @@ const inputId = `touch-${Math.random().toString(36).substring(7)}`;
   transition: height 0.4s ease;
 }
 :checked + #menu {
-  height: 6rem;
+  height: 5rem;
 }
 </style>
-
-<!-- <style scoped>
-#dropdown > #menu {
-  height: 0;
-  opacity: 0;
-  transition: opacity 0s linear;
-}
-#dropdown:hover > #menu {
-  margin-bottom: 2rem;
-  height: fit-content;
-  opacity: 1;
-  transition-duration: 0.5s;
-}
-</style> -->
