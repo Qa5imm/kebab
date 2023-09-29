25635560fdfc74bda700d58894fe30945830736e
Qa5im <24100256@lums.edu.pk>
2023-09-30 04:27:29 +0500

    meta tags added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,24 @@ export default defineNuxtConfig({
     baseURL: "/",
     buildAssetsDir: "assets",
     head: {
+      title: "Bike Chaiye",
+      meta: [
+        {
+          name: "description",
+          content:
+            "Welcome to Bike Chahiye, your go-to motorbike rental service located at LUMS! 🏍️ We offer a thrilling and convenient way to explore the campus and beyond",
+        },
+        {
+          name: "keywords",
+          content:
+            "Bike, Rent, Chaiye, bikeChaiye, chatnbot, first, service, startup, renting, bike renting",
+        },
+        {
+          name: "robots",
+          content:
+            "index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1",
+        },
+      ],
       link: [
         { rel: "icon", type: "image/x-icon", href: "images/bikeChaiye.jpg" },
       ],
