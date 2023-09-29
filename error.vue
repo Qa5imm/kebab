e50bd18e656e29e03bae0e469e8fe42fc0c9cb0f
Qa5im <24100256@lums.edu.pk>
2023-09-30 04:27:44 +0500

    error page added

---
+++ error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="flex flex-col items-center border-4 border-bred-200 mt-12 mx-auto w-fit px-24 py-12 gap-y-3"
+  >
+    <p class="font-bold text-4xl">{{ error.statusCode }}</p>
+    <p>Oops :(</p>
+    <p>{{ error.message }}</p>
+    <nuxt-link to="/" class="font-bold hover:underline px-2 py-1 rounded-md">Back to BikeChaiye?</nuxt-link>
+  </div>
+</template>
+<script setup>
+defineProps(["error"]);
+</script>
