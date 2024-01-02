4b17581b027cd77eecb4bb15373518aa714a927d
Qa5im <24100256@lums.edu.pk>
2024-01-02 21:07:53 +0500

    fixed design bug in rent

--- components/RentaBike/RentaBike.vue
+++ components/RentaBike/RentaBike.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="rentabike" class="flex flex-col justify-center items-center">
     <SectionHeader sectHeader="RENT A BIKE" sectHeadline="How Does It Work" />
-    <div class="flex flex-col items-center">
+    <div class="flex flex-col">
       <RentaBikeStep
         v-for="item in data"
         :img="item.img"
